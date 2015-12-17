65ee35ece74a261fc3397d05d2d8d4800be2d8cc
Hubert Kario <hubert@kario.pl>
2015-12-17 18:33:06 +0100

    remove trailing whitespace

--- fsresck/errors.py
+++ fsresck/errors.py
@@ -1,22 +1,22 @@
-#                                                                               
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          
-#                                                                               
-#   Copyright (c) 2015 Hubert Kario. All rights reserved.                       
-#                                                                               
-#   This copyrighted material is made available to anyone wishing               
-#   to use, modify, copy, or redistribute it subject to the terms               
-#   and conditions of the GNU General Public License version 2.                 
-#                                                                               
-#   This program is distributed in the hope that it will be                     
-#   useful, but WITHOUT ANY WARRANTY; without even the implied                  
-#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR                     
-#   PURPOSE. See the GNU General Public License for more details.               
-#                                                                               
-#   You should have received a copy of the GNU General Public                   
-#   License along with this program; if not, write to the Free                  
-#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,                 
-#   Boston, MA 02110-1301, USA.                                                 
-#                                                                               
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2015 Hubert Kario. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """ fsresck exceptions """
